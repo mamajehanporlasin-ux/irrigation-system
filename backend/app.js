7e4d89ccc0999811cb8ba21c36cbbd6c3ccdb845
Sy, Renz Anthony <sy.renz.anthony@gmail.com>
2025-12-04 16:38:48 +0800

    Added database functionality

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,25 @@
 import express from 'express';
+import { dbConnection } from './config/db_access.js';
+import { config as dotenvConfig } from 'dotenv';
+import fs from 'fs';
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+const secretPath =
+  fs.existsSync('/etc/secrets/.env')
+    ? '/etc/secrets/.env'
+    : './.env';
+
+dotenvConfig({ path: secretPath });
+
 
 app.get("/", (req, res)=>{
     res.json({message: "Server is working!"})
 });
 
 app.listen(PORT, ()=>{
+    dbConnection();
     console.log("server started at http://localhost:"+PORT);
 });
