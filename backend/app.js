14db73996c27e1dce80f777f3405e4d74f78e27e
Renz Anthony Sy <sy.renz.anthony@gmail.com>
2025-12-23 18:39:18 +0800

    fixing synchronization errors

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,6 @@ import { dbConnection } from './config/db_access.js';
 import { config as dotenvConfig } from 'dotenv';
 import fs from 'fs';
 import deviceRouter from './routers/device.router.js';
-<<<<<<< HEAD
 import userRouter from './routers/user.router.js';
 import checkOfflineDevices from './functions/checkOfflineDevices.js';
 import cors from 'cors';
@@ -23,29 +22,11 @@ app.use(cors({
   credentials: true
 }));
 
-=======
-import checkOfflineDevices from './functions/checkOfflineDevices.js';
-import cors from 'cors';
-
-const app = express();
-app.use(express.json());
-
-app.use(cors({
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  credentials: true,
-}));
-
-
->>>>>>> f400d892dd942d18ad749483142ac48e37a687c5
 const PORT = process.env.PORT || 5000;
 const secretPath =
   fs.existsSync('/etc/secrets/.env')
     ? '/etc/secrets/.env'
     : './.env';
-<<<<<<< HEAD
 
 dotenvConfig({ path: secretPath });
 
@@ -54,14 +35,6 @@ app.use("/api/user", userRouter);
 app.get("/", (req, res)=>{
     res.json({message: "Server is working!"})
 });
-=======
-dotenvConfig({ path: secretPath });
-
-app.get("/", (req, res)=>{
-    res.json({message: "Server is working!"})
-});
-app.use("/device", deviceRouter);
->>>>>>> f400d892dd942d18ad749483142ac48e37a687c5
 
 setInterval(checkOfflineDevices, 30000);
 
